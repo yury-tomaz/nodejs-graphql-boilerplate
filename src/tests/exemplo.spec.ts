bc2d8c1e0c14566746c773443908131bc022f919
Ciméria <cimeria@iMac-de-Cimeria.local>
2023-07-02 10:07:33 -0300

    feat: adicionei o jest e inclui um teste de exemplo

---
+++ src/tests/exemplo.spec.ts
@@ -0,0 +1,10 @@
+describe("TESTE", () =>{
+    it("Deve verificar a soma de dois numeros", () => {
+        let a = 1;
+        let b = 2;
+
+        const soma = a + b; 
+
+        expect(soma).toBe(3)
+    })
+})
